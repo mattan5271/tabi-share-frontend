d6e996188908bdd1118afd1196181dbbe632303e
mattan5271 <mattan.system.engineering@gmail.com>
2022-03-30 22:35:25 +0900

    ♻️ NextLinkコンポーネントのTS化完了

--- src/components/other/NextLink.tsx
+++ src/components/other/NextLink.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link";
+import { ReactNode, VFC } from "react";
+
 import { Link as ChakraLink } from "@chakra-ui/react";
 
-export const NextLink = (props) => {
+type Props = {
+  children: ReactNode;
+  href: string;
+};
+
+export const NextLink: VFC<Props> = (props) => {
   return (
     <Link href={props.href} passHref>
       <ChakraLink color={"blue.600"}>{props.children}</ChakraLink>
