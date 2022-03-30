f0ba77e7aab18d9047cd2695fb8f6d80dab65608
mattan5271 <mattan.system.engineering@gmail.com>
2022-03-30 22:38:34 +0900

    ♻️ NextLinkButtonコンポーネントのTS化完了

--- src/components/other/NextLinkButton.tsx
+++ src/components/other/NextLinkButton.tsx
@@ -1,7 +1,17 @@
 import Link from "next/link";
+import { ReactElement, ReactNode, VFC } from "react";
+
 import { Button } from "@chakra-ui/react";
 
-export const NextLinkButton = (props) => {
+type Props = {
+  children: ReactNode;
+  href: string;
+  colorScheme: string;
+  icon: ReactElement;
+  onClick?: () => void;
+};
+
+export const NextLinkButton: VFC<Props> = (props) => {
   return (
     <Link href={props.href} passHref>
       <Button as={"a"} colorScheme={props.colorScheme} leftIcon={props.icon} onClick={props.onClick}>
