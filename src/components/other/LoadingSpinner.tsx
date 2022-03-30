b321651f64ff43137cd7b61738ce777327da02eb
mattan5271 <mattan.system.engineering@gmail.com>
2022-03-30 22:34:08 +0900

    ♻️ LoadingSpinnerコンポーネントのTS化完了

--- src/components/other/LoadingSpinner.tsx
+++ src/components/other/LoadingSpinner.tsx
@@ -1,6 +1,12 @@
+import { VFC } from "react";
+
 import { VStack, Spinner, Text } from "@chakra-ui/react";
 
-export const LoadingSpinner = (props) => {
+type Props = {
+  text: string;
+};
+
+export const LoadingSpinner: VFC<Props> = (props) => {
   return (
     <VStack h={300} justify="center">
       <Text>{props.text}</Text>
